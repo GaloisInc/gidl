7a6d129f87caaf8a318f8f4d9cc281315beaff36
Adam C. Foltzer <acfoltzer@galois.com>
2017-03-01 16:26:59 -0800

    add subscription-based polling interface for GCS

--- src/Gidl/Backend/Elm/Interface.hs
+++ src/Gidl/Backend/Elm/Interface.hs
@@ -59,7 +59,8 @@ interfaceModule mp i@(Interface rawName _ _) =
                  , "import Http"
                  , "import Json.Decode"
                  , "import Json.Encode"
-                 , "import Time"
+                 , "import Task"
+                 , "import Time exposing (Time)"
                  ]
 
 data ClientMessage
@@ -89,67 +90,90 @@ clientIfDecl _ _ interfaceName [] = stack $
       , "unused = True"
     ]
 clientIfDecl tmp rawName interfaceName ms = stack $
-    [ "{-| A type containing all of the fields of the interface,"
+    [ "{-| A type containing all of the fields and poll rates of the interface,"
       <+> "useful for keeping state in a model -}"
     , "type alias" <+> text interfaceName <+> equals
     , indent 2 $ encloseStack lbrace rbrace comma $
-        [ text (toCamel n) <+> colon <+> elmTypeQName tmp t
-        | (GetMessage n t) <- ms
-        ]
+        concat [ [ text (toCamel n) <+> colon <+> elmTypeQName tmp t
+                 , pollRateName n <+> colon <+> "Maybe Time" ]
+               | (GetMessage n t) <- ms ]
     , empty
     , "{-|" <+> text interfaceName
       <+> "initialized with (arbitrary) default values -}"
     , "init" <+> colon <+> text interfaceName
     , "init" <+> equals
     , indent 2 $ encloseStack lbrace rbrace comma $
-        [ text (toCamel n) <+> equals <+> typeInit tmp t
-        | (GetMessage n t) <- ms
-        ]
+        concat [ [ text (toCamel n) <+> equals <+> typeInit tmp t
+                 , pollRateName n <+> equals <+> "Nothing" ]
+               | (GetMessage n t) <- ms ]
     , empty
       -- client interface
     , "{-| Client interface for" <+> text interfaceName <+> "backed by a"
       <+> "Gidl-generated RPC server -}"
     , "type alias" <+> "Client" <+> "msg" <+> equals
     , indent 2 $ encloseStack lbrace rbrace comma $
-        [ getFieldName n <+> colon <+> "Cmd msg"
-        | (GetMessage n _) <- ms
-        ] ++
+        concat [ [ getFieldName n <+> colon <+> "Cmd msg"
+                 , pollFieldName n <+> colon
+                     <+> "Maybe Time" <+> "->" <+> "Cmd msg" ]
+               | (GetMessage n _) <- ms ] ++
         [ setFieldName n <+> colon
           <+> elmTypeQName tmp t <+> "->" <+> "Cmd msg"
         | (SetMessage n t) <- ms
         ]
     , empty
-      -- response variants for interpretation by Handler
-    , "{-| Response variants. Not recommended for use directly, instead"
+      -- Msg variants for interpretation by Handler
+    , "{-| Msg variants. Not recommended for use directly, instead"
       <+> "use the `Handler` interface -}"
-    , "type" <+> "Response"
+    , "type" <+> "Msg"
     , indent 2 $ encloseStack equals empty (text "|") $
-        [ getResponseConstr n <+> elmTypeQName tmp t
-        | (GetMessage n t) <- ms ] ++
-        [ setResponseConstr n
-        | (SetMessage n _) <- ms ]
+        concat [ [ getMsgConstr n <+> elmTypeQName tmp t
+                 , pollMsgConstr n
+                 , pollRateMsgConstr n <+> parens "Maybe Time" ]
+               | (GetMessage n t) <- ms ] ++
+        [ setMsgConstr n | (SetMessage n _) <- ms ]
+      -- helper to determine whether a Msg originated from a network call
+    , "{-| Determine whether a `Msg` arose from a network call -}"
+    , "networkMsg" <+> colon <+> "Msg" <+> "->" <+> "Bool"
+    , "networkMsg" <+> "msg" <+> equals
+    , indent 2 $ stack $
+        [ "case msg of"
+        , indent 2 $ stack $
+          [ getMsgConstr n <+> "_" <+> "->" <+> "True"
+          | (GetMessage n _) <- ms ] ++
+          [ setMsgConstr n <+> "->" <+> "True"
+          | (SetMessage n _) <- ms ] ++
+          [ "_ -> False" ]
+        ]
+    , empty
       -- function for building a client
     , "{-| Given an error handler, a builder for your `msg` type, and a"
       <+> "base URL for the RPC server, build a `Client` -}"
     , "client" <+> colon
       <+> "Time.Time" <+> "->"
       <+> "(Http.Error -> msg)" <+> "->"
-      <+> parens ("Response" <+> "->" <+> "msg") <+> "->"
+      <+> parens ("Msg" <+> "->" <+> "msg") <+> "->"
       <+> "String" <+> "->"
       <+> "Client" <+> "msg"
     , "client" <+> "to" <+> "err" <+> "ok" <+> "url" <+> equals
     , indent 2 $ encloseStack lbrace rbrace comma $
         [ getFieldName n <+> equals
           <+> "Utils.send_" <+> "err"
-          <+> parens ("ok" <+> "<<" <+> getResponseConstr n)
+          <+> parens ("ok" <+> "<<" <+> getMsgConstr n)
           <+> parens ("Utils.get_" <+> "to"
             <+> parens ("url" <+> "++"
               <+> dquotes ("/" <> text rawName <> "/" <> text n))
             <+> typeDecoder tmp t)
         | (GetMessage n t) <- ms ] ++
+        [ pollFieldName n <+> equals <+> "\\mt ->"
+          <+> "Task.perform"
+          <+> parens ("always"
+            <+> parens ("ok"
+              <+> parens (pollRateMsgConstr n <+> "mt")))
+          <+> parens ("Task.succeed" <+> "()")
+        | (GetMessage n _) <- ms ] ++
         [ setFieldName n <+> equals <+> "\\x ->"
           <+> "Utils.send_" <+> "err"
-          <+> parens ("ok" <+> "<<" <+> "always" <+> setResponseConstr n)
+          <+> parens ("ok" <+> "<<" <+> "always" <+> setMsgConstr n)
           <+> parens ("Utils.post_" <+> "to"
             <+> parens ("url" <+> "++"
               <+> dquotes ("/" <> text rawName <> "/" <> text n))
@@ -167,6 +191,12 @@ clientIfDecl tmp rawName interfaceName ms = stack $
         [ handleGet n <+> colon
           <+> elmTypeQName tmp t <+> "-> model -> (model, Cmd msg)"
         | (GetMessage n t) <- ms ] ++
+        [ handlePoll n <+> colon
+          <+> "(model -> Client msg) -> model -> (model, Cmd msg)"
+        | (GetMessage n _) <- ms ] ++
+        [ handlePollRate n <+> colon
+          <+> "Maybe Time -> model -> (model, Cmd msg)"
+        | (GetMessage n _) <- ms ] ++
         [ handleSet n <+> colon <+> "model -> (model, Cmd msg)"
         | (SetMessage n _) <- ms ]
     , empty
@@ -198,32 +228,87 @@ clientIfDecl tmp rawName interfaceName ms = stack $
                   ("i |" <+> text (toCamel n) <+> equals <+> "x"))
              <> comma <+> "Cmd.none")
         | (GetMessage n _) <- ms ] ++
+        [ handlePoll n <+> equals
+          <+> "\\cl m ->"
+          <+> parens ("m" <+> comma
+            <+> parens ("cl m") <> dot <> getFieldName n)
+        | (GetMessage n _) <- ms ] ++
+        [ handlePollRate n <+> equals
+          <+> "\\mt m ->"
+          <+> parens
+            ("upd m"
+             <+> parens
+               ("\\i ->" <+> braces
+                  ("i |" <+> pollRateName n <+> equals <+> "mt"))
+               <> comma <+> "Cmd.none")
+        | (GetMessage n _) <- ms ] ++
         [ handleSet n <+> equals <+> "\\m -> (m, Cmd.none)"
         | (SetMessage n _) <- ms ]
     , empty
       -- Handler driver
     , "{-| Driver for a `Handler`; use in your `update` function -}"
-    , "handle : Handler model msg -> Response -> model -> (model, Cmd msg)"
-    , "handle" <+> "h" <+> "r" <+> "m" <+> equals
+    , "handle" <+> colon
+      <+> "Handler model msg -> (model -> Client msg)"
+      <+> "-> Msg -> model -> (model, Cmd msg)"
+    , "handle" <+> "h" <+> "cl" <+> "r" <+> "m" <+> equals
     , indent 2 $ stack $
         [ "case r of"
         , indent 2 $ stack $
-            [ getResponseConstr n <+> "x" <+> "->"
+            [ getMsgConstr n <+> "x" <+> "->"
               <+> "h" <> dot <> handleGet n <+> "x" <+> "m"
             | (GetMessage n _) <- ms ] ++
-            [ setResponseConstr n <+> "->"
+            [ pollMsgConstr n <+> "->"
+              <+> "h" <> dot <> handlePoll n <+> "cl" <+> "m"
+            | (GetMessage n _) <- ms ] ++
+            [ pollRateMsgConstr n <+> "mt" <+> "->"
+              <+> "h" <> dot <> handlePollRate n <+> "mt" <+> "m"
+            | (GetMessage n _) <- ms ] ++
+            [ setMsgConstr n <+> "->"
               <+> "h" <> dot <> handleSet n <+> "m"
             | (SetMessage n _) <- ms
             ]
         ]
+    , empty
+      -- Subscription generator
+    , "{-| Generate subscriptions from the poll rates in the `"
+      <> text interfaceName <> "` -}"
+    , "subscriptions" <+> colon
+      <+> text interfaceName <+> "->"
+      <+> parens "Msg -> msg" <+> "->"
+      <+> "List (Sub.Sub msg)"
+    , "subscriptions" <+> "i" <+> "toMsg" <+> equals
+    , indent 2 $ encloseStack lbracket rbracket comma $
+        [ "Maybe.withDefault Sub.none" <+> parens
+            ("Maybe.map" <+> parens
+              ("\\t -> Time.every t" <+> parens
+                ("always" <+> parens ("toMsg" <+> pollMsgConstr n)))
+              <+> "i" <> dot <> pollRateName n)
+        | (GetMessage n _) <- ms ]
+    , empty
+    , "{-| Map over the polling rates that are not disabled -}"
+    , "mapPollRates" <+> colon <+> parens "Time -> Time" <+> "->"
+      <+> text interfaceName <+> "->" <+> text interfaceName
+    , "mapPollRates f i ="
+    , indent 2 $ braces ("i" <+>
+        indent 2 (encloseStack "|" empty comma $
+          [ pollRateName n <+> equals
+            <+> "Maybe.map f" <+> "i" <> dot <> pollRateName n
+          | (GetMessage n _) <- ms ]))
     ]
   where
   handleGet n = "handleGot" <> text (toCapped n)
   handleSet n = "handleSet" <> text (toCapped n)
-  getResponseConstr n = "Got" <> text (toCapped n)
-  setResponseConstr n = "Set" <> text (toCapped n)
+  handlePoll n = "handlePoll" <> text (toCapped n)
+  handlePollRate n = "handlePollRate" <> text (toCapped n)
+  getMsgConstr n = "Got" <> text (toCapped n)
+  setMsgConstr n = "Set" <> text (toCapped n)
+  pollMsgConstr n = "Poll" <> text (toCapped n)
+  pollRateMsgConstr n = "PollRate" <> text (toCapped n)
   getFieldName n = "get" <> text (toCapped n)
   setFieldName n = "set" <> text (toCapped n)
+  pollFieldName n = "poll" <> text (toCapped n)
+  pollRateName n = text (toCamel n) <> "PollRate"
+
 
 -- | Similar to 'toCapped', but ignores a trailing @_i@
 ifModuleName :: Interface -> String
